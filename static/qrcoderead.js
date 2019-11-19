10dd3cd543c6cacb8403cf2ccf44fa2ea2a18cf1
jackrmanuel <37941025+jackrmanuel@users.noreply.github.com>
2019-11-19 11:38:56 -0800

    qrcode reader java script

---
+++ static/qrcoderead.js
@@ -0,0 +1,14 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+  document.getElementById('form:accountnumber').value =content; 
+  document.getElementById("form:button").click();
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[0]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
